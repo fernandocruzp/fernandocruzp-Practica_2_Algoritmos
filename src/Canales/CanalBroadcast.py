5db68b42db1cf59c44011ca100e05daaf7e371cb
fernandocruzp <cruzfernando@ciencias.unam.mx>
2024-09-21 20:50:24 -0600

    asfa

---
+++ src/Canales/CanalBroadcast.py
@@ -0,0 +1,27 @@
+import simpy
+from Canales.Canal import Canal
+
+
+class CanalBroadcast(Canal):
+    '''
+    Clase que modela un canal, permite enviar mensajes one-to-many.
+    '''
+
+    def __init__(self, env, capacidad=simpy.core.Infinity):
+        self.env = env
+        self.capacidad = capacidad
+        self.canales = []
+
+    def envia(self, mensaje, vecinos):
+        '''
+        Envia un mensaje a los canales de salida de los vecinos.
+        '''
+        # Tu código aquí
+
+    def crea_canal_de_entrada(self):
+        '''
+        Creamos un canal de entrada
+        '''
+        canal_entrada = simpy.Store(self.env, capacity=self.capacidad)
+        self.canales.append(canal_entrada)
+        return canal_entrada
