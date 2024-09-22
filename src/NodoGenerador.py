3fe191b23371f0580e886bff4a112d2531929e49
Yael R. <yaelxi@ciencias.unam.mx>
2024-09-22 11:31:10 -0600

    Árbol a tuplas

--- src/NodoGenerador.py
+++ src/NodoGenerador.py
@@ -12,12 +12,12 @@ class NodoGenerador(Nodo):
         self.vecinos = vecinos
         self.canal_entrada = canal_entrada
         self.canal_salida = canal_salida
-        
+
         # Atributos propios del algoritmo
-        self.padre = None if id_nodo != 0 else id_nodo # Si es el nodo distinguido, el padre es el mismo 
+        self.padre = None if id_nodo != 0 else id_nodo # Si es el nodo distinguido, el padre es el mismo
         self.hijos = list()
         self.mensajes_esperados = len(vecinos) # Cantidad de mensajes que esperamos
-    
+
     def genera_arbol(self, env):
         '''
         Si el nodo es el distinguido (id_nodo=0), envía un mensaje inicial a sus vecinos.
@@ -29,28 +29,31 @@ class NodoGenerador(Nodo):
         '''
         if self.id_nodo == 0:
             # El nodo distinguido envía un mensaje inicial "GO" a sus vecinos
-            env.process(self.canal_salida.envia("GO," + str(self.id_nodo), self.vecinos))
-        
+            go_nodo = ("GO", self.id_nodo)
+            env.process(self.canal_salida.envia(go_nodo, self.vecinos))
+
         while True:
-            mensaje_recibido = yield self.canal_entrada.get()
-            mensaje = mensaje_recibido.split(",")
-            accion = mensaje[0]
-            id_recibido = int(mensaje[1]) if mensaje[1] != "None" else None
+            # mensaje_recibido = yield self.canal_entrada.get()
+            mensaje = yield self.canal_entrada.get()
+            accion, id_recibido = mensaje
 
             if accion == "GO":
                 if self.padre is None:
                     # Si el nodo no tiene padre, el nodo remitente se convierte en su padre
                     self.padre = id_recibido
-                    self.mensajes_esperados -= 1 
+                    self.mensajes_esperados -= 1
                     if self.mensajes_esperados == 0:
                         # Si ya no espera más mensajes, envía un mensaje "Back" a su padre
-                        env.process(self.canal_salida.envia("Back," + str(self.id_nodo), [self.padre]))
+                        back_nodo = ("Back", self.id_nodo)
+                        yield env.process(self.canal_salida.envia(back_nodo, [self.padre]))
                     else:
                         # Reenvía "GO" a sus vecinos, excepto el padre
-                        env.process(self.canal_salida.envia("GO," + str(self.id_nodo), [x for x in self.vecinos if x != self.padre]))
+                        go_nodo = ("GO", self.id_nodo)
+                        env.process(self.canal_salida.envia(go_nodo, [x for x in self.vecinos if x != self.padre]))
                 else:
                     # Si ya tiene padre, responde con "Back" indicando que el nodo remitente no es su padre
-                    env.process(self.canal_salida.envia("Back,None", [id_recibido]))
+                    back_none = ("Back", None)
+                    env.process(self.canal_salida.envia(back_none, [id_recibido]))
 
             elif accion == "Back":
                 self.mensajes_esperados -= 1
@@ -60,6 +63,7 @@ class NodoGenerador(Nodo):
                 if self.mensajes_esperados == 0 and self.padre is not None:
                     # Si ya no espera mensajes y tiene padre, envía "Back" a su padre
                     if self.id_nodo != 0:
-                        env.process(self.canal_salida.envia("Back," + str(self.id_nodo), [self.padre]))
+                        back_nodo = ("Back", self.id_nodo)
+                        env.process(self.canal_salida.envia( back_nodo, [self.padre]))
 
             yield env.timeout(TICK)
